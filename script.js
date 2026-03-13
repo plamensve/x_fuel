c718a0dc0f309c5cf9cb8cbaa86e6cd0f378903e
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-03-13 20:08:23 +0200

    ADD: two themes

--- script.js
+++ script.js
@@ -352,3 +352,32 @@ document.addEventListener("DOMContentLoaded", function () {
     loadPrices()
 
 })
+
+
+
+let toggle = document.getElementById("theme-toggle")
+
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light")
+    toggle.textContent = "☀️"
+}else{
+    toggle.textContent = "🌙"
+}
+
+toggle.addEventListener("click", function(){
+
+    document.body.classList.toggle("light")
+
+    if(document.body.classList.contains("light")){
+
+        localStorage.setItem("theme","light")
+        toggle.textContent = "☀️"
+
+    }else{
+
+        localStorage.setItem("theme","dark")
+        toggle.textContent = "🌙"
+
+    }
+
+})
